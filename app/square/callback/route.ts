360c4241cee23ad516cfb4ea7dc0c94b084c275f
Your Name <ishmamp3@gmail.com>
2025-11-26 15:13:30 -0800

    fixing auto sign out

--- app/square/callback/route.ts
+++ app/square/callback/route.ts
@@ -71,7 +71,8 @@ export async function GET(request: NextRequest) {
             secure: process.env.NODE_ENV === "production",
             httpOnly: true,
             sameSite: "lax",
-            maxAge: 60 * 60 * 24 * 30, // FIXED: 30 Days
+            maxAge: 60 * 60 * 24 * 30, // 30 Days
+            path: "/", // <--- ADD THIS LINE!
         });
 
         return NextResponse.redirect(new URL("/dashboard?status=connected", request.url));
